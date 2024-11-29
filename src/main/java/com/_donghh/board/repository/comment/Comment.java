0fce033a7668f1f29d7d4f8475478fa6f8def8a8
이동혁 <rirws96@naver.com>
2024-11-29 11:47:49 +0900

    feat: 댓글 엔티티 설계

---
+++ src/main/java/com/_donghh/board/repository/comment/Comment.java
@@ -0,0 +1,31 @@
+package com._donghh.board.repository.comment;
+
+import com._donghh.board.repository.board.Board;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id")
+    private Board board;
+
+    @Column(nullable = false)
+    private String content;
+}
